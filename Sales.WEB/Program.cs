f9863a7f47115c22280dc31ef09ed4aaa5e755fe
teo <mateoarroyave259716@correo.itm.edu.co>
2023-03-02 00:34:20 -0500

    Code Project

---
+++ Sales.WEB/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Sales.WEB;
+using Sales.WEB.Repositories;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7269/") });
+builder.Services.AddScoped<IRepository, Repository>();
+
+await builder.Build().RunAsync();
+
