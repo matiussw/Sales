96e6b6863e5833105e82b059e69094270cbaf5a5
teo <mateoarroyave259716@correo.itm.edu.co>
2023-03-03 03:33:50 -0500

    Category Structure

--- Sales.API/Data/SeedDb.cs
+++ Sales.API/Data/SeedDb.cs
@@ -18,6 +18,7 @@ namespace Sales.API.Data
         {
             await _context.Database.EnsureCreatedAsync();
             await CheckCountriesAsync();
+         //   await CheckCategoriesAsync();
         }
 
         private async Task CheckCountriesAsync()
@@ -33,7 +34,26 @@ namespace Sales.API.Data
                 _context.Countries.Add(new Country { Name = "Argentina" });
                 _context.Countries.Add(new Country { Name = "Canada" });
             }
+            await _context.SaveChangesAsync();
+        }
 
+        private async Task CheckCategoriesAsync()
+        {
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.Add(new Category { Name = "Zapatos" });
+                _context.Categories.Add(new Category { Name = "Camisas" });
+                _context.Categories.Add(new Category { Name = "Juguetes" });
+                _context.Categories.Add(new Category { Name = "Interiores" });
+                _context.Categories.Add(new Category { Name = "Relojes" });
+                _context.Categories.Add(new Category { Name = "Gafas" });
+                _context.Categories.Add(new Category { Name = "Gorras" });
+                _context.Categories.Add(new Category { Name = "Comida" });
+                _context.Categories.Add(new Category { Name = "Medias" });
+                _context.Categories.Add(new Category { Name = "Joyas" });
+                _context.Categories.Add(new Category { Name = "Tecnologia" });
+                _context.Categories.Add(new Category { Name = "Vajillas" });
+            }
             await _context.SaveChangesAsync();
         }
 
---
+++ Sales.Shared/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Sales.Shared.Entities
+{
+    public class Category
+	{
+        public int Id { get; set; }
+
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "El Campo {0} es Obligatorio")]
+        [MaxLength(100, ErrorMessage = "El Campo {0} no puede tener mas de un {1} Caracteres")]
+
+        public String Name { get; set; } = null!;
+    }
+}
+
